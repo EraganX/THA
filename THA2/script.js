4ba49b60fc10c46ab8f59fee3f7a1501f2914721
Charith Eranga <91379491+Eranga-DC@users.noreply.github.com>
2022-09-16 22:21:23 +0530

    THA2

---
+++ THA2/script.js
@@ -0,0 +1,23 @@
+document.getElementById("btn1").addEventListener("click", function() {
+    alert("Marino Beach booked");
+  }, true);
+
+document.getElementById("btn2").addEventListener("click", function() {
+    alert("Shangri-La booked");
+}, true);
+
+document.getElementById("btn2").addEventListener("click", function() {
+    alert("Shangri-La booked");
+}, true);
+
+document.getElementById("btn3").addEventListener("click", function() {
+    alert("Pearl Grand booked");
+}, true);
+
+document.getElementById("btn4").addEventListener("click", function() {
+    alert("55TG Boutique Suites booked");
+}, true);
+
+document.getElementById("btn5").addEventListener("click", function() {
+    alert("Hotel MaRadha booked");
+}, true);
